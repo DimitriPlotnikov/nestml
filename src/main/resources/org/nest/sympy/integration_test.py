69de940d80c8d01020f459ee5fb473f2a4a0d69f
DimitriPlotnikov <d.plotnikov@fz-juelich.de>
2017-04-21 16:00:30 +0200

    Add a management class to control solver selection. Start factoring out reusable parts. Add an JSON output of the algorithm result.

--- src/main/resources/org/nest/sympy/integration_test.py
+++ src/main/resources/org/nest/sympy/integration_test.py
@@ -1,9 +1,12 @@
-from sympy import *
-import prop_matrix as otpm
 import unittest
-from shapes import ShapeFunction, ShapeODE
+
+from prop_matrix import PropagatorCalculator
+from shapes import ShapeFunction
+from OdeAnalyzer import OdeAnalyzer
+
 
 class TestShapesConversionAndPropagatorMatrxiComputation(unittest.TestCase):
+
     def test_shape_conversion(self):
         shape_alpha = ShapeFunction("shape_alpha", "e / tau * t * exp(-t / tau)")
         self.assertEqual(2, shape_alpha.order)
@@ -11,7 +14,7 @@ class TestShapesConversionAndPropagatorMatrxiComputation(unittest.TestCase):
         shape_exp = ShapeFunction("shape_exp", "exp(-t/tau)")
         self.assertEqual(1, shape_exp.order)
 
-        shape_1 = ShapeFunction("shape_1",  "e / tau * t * t * exp(-t / tau)")
+        shape_1 = ShapeFunction("shape_1", "e / tau * t * t * exp(-t / tau)")
         self.assertEqual(3, shape_1.order)
         shape_3 = ShapeFunction("shape_3", "exp(-t/tau) + e / tau * t * exp(-t / tau)")
         self.assertEqual(2, shape_3.order)
@@ -23,16 +26,26 @@ class TestShapesConversionAndPropagatorMatrxiComputation(unittest.TestCase):
         self.assertEqual(2, shape_5.order)
 
     def test_propagator_matrix(self):
-        shape_alpha = ShapeFunction("shape_alpha", "e / tau * t * exp(-t / tau)")
-        shape_exp = ShapeFunction("shape_exp", "exp(-t/tau)")
-        shape_sin = ShapeFunction("shape_sinb", "sin(t)")
-        shapes = [shape_alpha, shape_exp, shape_sin]
+        shape_inh = ShapeFunction("I_in", "(e/tau_syn_in) * t * exp(-t/tau_syn_in)")
+        shape_exc = ShapeFunction("I_ex", "(e/tau_syn_ex) * t * exp(-t/tau_syn_ex)")
+        shapes = [shape_inh, shape_exc]
 
         ode_var = "V_m"
-        ode_rhs = "-1/Tau * V_m-1/C * (shape_alpha + shape_exp + shape_sin + currents + I_E)"
-        prop_matrices, const_term, step_const = otpm.ode_to_prop_matrices(shapes, ode_var, ode_rhs)
+        ode_rhs = "-V_m/Tau + (I_in + I_ex + I_e) / C_m"
 
-        pm, ps = otpm.prop_matrix_to_prop_step(prop_matrices, const_term, step_const, shapes, ode_var)
-        
+        calculator = PropagatorCalculator()
+        prop_matrices, const_input, step_const = calculator.ode_to_prop_matrices(shapes, ode_var, ode_rhs)
+        propagator_elements, ode_var_factor, const_input, update_instructions \
+            = calculator.prop_matrix_to_prop_step(prop_matrices, step_const, shapes, ode_var)
+        self.assertTrue(len(propagator_elements) > 0)
+
+    def test_analyzer(self):
+        testant = OdeAnalyzer.compute_solution(
+            ["V_m' = -V_m/Tau + (I_in + I_ex + I_e) / C_m",
+             "I_in = (e/tau_syn_in) * t * exp(-t/tau_syn_in)",
+             "I_ex = (e/tau_syn_ex) * t * exp(-t/tau_syn_ex)"])
+        self.assertIsNotNone(testant)
+        print testant
+
 if __name__ == '__main__':
     unittest.main()
